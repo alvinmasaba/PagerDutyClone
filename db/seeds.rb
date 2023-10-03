609524518a87c3684046d51bb66c181584e4ea7b
Alvin <alvinmasaba@gmail.com>
2023-10-03 03:11:16 -0400

    Fixes seeds bug.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Incident.destroy_all
+TeamMember.destroy_all
+
+#Create 5 team members
+5.times do
+  TeamMember.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    number: Faker::PhoneNumber.cell_phone_in_e164,
+    oncall: Faker::Boolean.boolean
+  )
+end
+
+# Fetch all team member IDs
+team_member_ids = TeamMember.pluck(:id)
+
+#Create 10 incidents
+10.times do
+  Incident.create!(
+    urgency: ['HIGH', 'MEDIUM', 'LOW'].sample,
+    triggered: Faker::Boolean.boolean,
+    acknowledged: Faker::Boolean.boolean,
+    resolved: Faker::Boolean.boolean,
+    description: Faker::Lorem.sentence,
+    assigned_to_id: team_member_ids.sample
+  )
+end
