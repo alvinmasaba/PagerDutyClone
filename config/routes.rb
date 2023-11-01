dfc9437ef5ac17fe038f534ff8a28b54ff02708b
Alvin <alvinmasaba@gmail.com>
2023-11-01 02:06:23 -0400

    Adds shifts routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :incidents
       resources :team_members
+      resources :shifts
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
